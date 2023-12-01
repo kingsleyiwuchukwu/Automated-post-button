757a6bd4b880d0bb5ab1905ec880cdcf6dc4aa8d
kingsleyiwuchukwu <kingsleyiwuchukwu0@gmail.com>
2023-12-01 02:10:23 +0100

    Updated Js Files

---
+++ scripts.js
@@ -0,0 +1,26 @@
+// Get access to DOM elements
+const header = document.querySelector('header');
+
+const addPostButton = document.getElementById('add-post');
+const articleSection = document.querySelector('section');
+
+addPostButton.addEventListener('click', () => {
+    const newPost = createNewPost();
+    articleSection.appendChild(newPost);
+});
+
+// Page functions
+function createNewPost() {
+  let newArticle = document.createElement('article');
+  let newHeading = document.createElement('h5');
+  let newParagraph = document.createElement('p');
+  
+  newHeading.textContent = 'Another blog post!';
+  newParagraph.textContent = 'Love me! gnaw the corn cob intently sniff hand meow for food, then when human fills food dish, take a few bites of food and continue meowing really likes hummus meow all night having their mate disturbing sleeping humans. Run in circles pose purrfectly to show my beauty for when owners are asleep, cry for no apparent reason sit by the fire or lies down or paw at your fat belly.';
+  
+  newArticle.appendChild(newHeading);
+  newArticle.appendChild(newParagraph);
+  
+  newArticle.classList.add('list-group-item');
+  return newArticle;
+}
